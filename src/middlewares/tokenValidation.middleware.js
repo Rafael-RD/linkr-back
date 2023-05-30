0d1a690caa59ff01ca21860eff0b9e5f327fbfae
Rafael <rafaelreinertd@hotmail.com>
2023-05-30 13:41:47 -0300

    fix: secret key

--- src/middlewares/tokenValidation.middleware.js
+++ src/middlewares/tokenValidation.middleware.js
@@ -4,10 +4,12 @@ export default function tokenValidation(req, res, next){
     const token=req.headers.authorization?.replace("Bearer ", "");
 
     try {
-        const tokenData=jwt.verify(token, process.env.JWT_SECRET);
+        const tokenData=jwt.verify(token, process.env.SECRET_KEY);
         res.locals.tokenData=tokenData;
         next();
     } catch (error) {
-        return res.sendStatus(401);
+        return res.sendStatus(error);
     }
 }
+
+console.log(tokenValidation({headers:{authorization: "Bearer safiuahsdfhiausdhfiuahsdf"}}, {sendStatus: (status)=>status}, ()=>console.log("foi")));
